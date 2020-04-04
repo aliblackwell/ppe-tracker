5dd0644df39a7802c2eed42b3ed487232162e532
Alasdair Blackwell <aliblackwell@protonmail.com>
2020-04-04 22:59:58 +0100

    Form validation

--- functions/handle-onboarding.js
+++ functions/handle-onboarding.js
@@ -4,32 +4,99 @@ const serverless = require("serverless-http")
 var multer = require("multer") // required for handling FormData objects
 var upload = multer()
 
+const crypto = require("crypto")
 const { body, validationResult } = require("express-validator")
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) // support encoded bodies
 const Cloudant = require("@cloudant/cloudant")
 const cloudant = Cloudant(
-  `https://${process.env.CLOUDANT_PARTICIPANTS_USER}:${process.env.CLOUDANT_PARTICIPANTS_}@${process.env.CLOUDANT_HOST}`
+  `https://${process.env.CLOUDANT_PARTICIPANTS_USER}:${process.env.CLOUDANT_PARTICIPANTS_PW}@${process.env.CLOUDANT_HOST}`
 )
 const db = cloudant.db.use("participants")
 
+function formatPhone(ac, phone) {
+  let phoneNoZero = parseInt(phone)
+  let cleanPhone = phoneNoZero.toString().replace(/ /g, "")
+  let cleanAc = ac.toString().replace(/ /g, "")
+  return `${cleanAc}${cleanPhone}`
+}
+
+function validateAnswer(answer){
+  return body(answer).not().isEmpty().withMessage('Please answer the question.')
+}
+
+function fieldRequired(field) {
+  return body(field).not().isEmpty().withMessage('This field is required.')
+}
+
 app.post(
-  "*",
+  "*", 
   [
     upload.none(), // enable req.body.{form[name]} below using multer
-    body("email").isEmail().normalizeEmail(),
-    body("phone").custom((p, { req }) => (req.body.phone = parseInt(p))) // remove leading zero if present
-    
+    validateAnswer('answer-one'),
+    validateAnswer('answer-two'),
+    validateAnswer('answer-three'),
+    fieldRequired('area-code'),
+    fieldRequired('phone'),
+    fieldRequired('hospital')
   ],
-  (req, res) => {
-    console.log(req.body.phone)
+  async (req, res, next) => {
     // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() })
     }
-    res.json({ message: "success" })
+
+    const phone = formatPhone(req.body["area-code"], req.body.phone)
+
+    const identifibleHash = crypto
+      .createHmac("sha256", process.env.SALT_ONE)
+      .update(phone)
+      .digest("hex")
+
+    const anonymousHash = crypto
+      .createHmac("sha256", process.env.SALT_TWO)
+      .update(phone)
+      .digest("hex")
+
+    const timeBaseIdentifier = crypto
+      .createHmac("sha256", process.env.SALT_THREE)
+      .update(Date.now().toString())
+      .digest("hex")
+
+    const mobiles = {
+      _id: `mobiles:${identifibleHash}`,
+      phone,
+    }
+
+    const user = {
+      _id: `user:${anonymousHash}`,
+      gender: req.body.gender,
+      nationality: req.body.nationality,
+      jobTitle: req.body["job-title"],
+    }
+
+    const answerBlock = {
+      _id: `answers:${timeBaseIdentifier}`,
+      user: anonymousHash,
+      timestamp: Date.now(), // make just day/month/year
+      answerOne: req.body["answer-one"],
+      answerTwo: req.body["answer-two"],
+      answerThree: req.body["answer-three"],
+      hospital: req.body.hospital,
+    }
+
+    const documents = {
+      docs: [mobiles, user, answerBlock],
+    }
+
+    try {
+      const success = await db.bulk(documents)
+      res.json({ message: "success", update: success })
+    } catch (error) {
+      return res.status(504).json({ error })
+    }
   }
 )
 
--- website/js/onboarding-form.js
+++ website/js/onboarding-form.js
@@ -1,11 +1,77 @@
 const form = document.querySelector("form#onboarding-form")
+const formWrapper = document.querySelector(".form-wrapper")
+const submitSuccess = document.querySelector(".submission-success")
+const loading = document.querySelector(".loading")
+const messages = document.querySelector(".messages")
+const errorList = document.querySelector("#error-list")
+
+function showEl(el) {
+  el.classList.remove("hidden")
+  el.setAttribute("aria-hidden", "false")
+}
+
+function hideEl(el) {
+  el.classList.add("hidden")
+  el.setAttribute("aria-hidden", "true")
+}
+
+function handleErrors(errors) {
+  let errorsClosed = 0
+  showEl(messages)
+  errors.map((error) => {
+    let element = document.querySelector(`#${error.param}`)
+    let parentId = element.getAttribute("data-parent")
+    const hasErr = element.getAttribute("data-has-err")
+    if (!hasErr) {
+      element.setAttribute("data-has-err", true)
+      let errorMessage = document.createElement("span")
+
+      let errorListItem = document.createElement("li")
+      errorListItem.setAttribute("class", `msg-${error.param}`)
+      let errorLink = document.createElement("a")
+      errorLink.innerHTML = error.param
+      errorLink.setAttribute("href", `#${parentId}`)
+      errorListItem.innerHTML = errorLink.outerHTML
+      errorList.insertAdjacentElement("beforeend", errorListItem)
+
+      errorMessage.classList.add(`msg`)
+      errorMessage.classList.add(`msg-${error.param}`)
+      errorMessage.setAttribute("role", "alert")
+      errorMessage.innerHTML = error.msg
+      element.insertAdjacentElement("beforebegin", errorMessage)
+      element.classList.add("error")
+      let addedMsg = document.querySelectorAll(`.msg-${error.param}`)
+      element.addEventListener("change", () => {
+        errorsClosed++
+        element.classList.remove("error")
+        element.setAttribute("data-has-err", false)
+        addedMsg.forEach((el) => el.remove())
+        if (errors.length === errorsClosed) {
+          hideEl(messages)
+        }
+      })
+    }
+  })
+}
+
 form.addEventListener("submit", (evt) => {
+  showEl(loading)
+  window.scrollTo(0, 0)
   evt.preventDefault()
+
   const FD = new FormData(form)
   fetch(form.getAttribute("action"), {
     method: "POST",
     body: FD,
   })
     .then((response) => response.json())
-    .then((result) => console.log(result))
+    .then((result) => {
+      hideEl(loading)
+      if (result.errors) {
+        handleErrors(result.errors)
+      } else {
+        hideEl(formWrapper)
+        showEl(submitSuccess)
+      }
+    })
 })
