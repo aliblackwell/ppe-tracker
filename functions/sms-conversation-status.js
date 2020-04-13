5915c013ce0a7a0c87ae23d8db3d522ed5d76d1b
Alasdair Blackwell <aliblackwell@protonmail.com>
2020-04-13 15:50:34 +0100

    Add SMS capability

--- functions/sms-conversation-status.js
+++ functions/sms-conversation-status.js
@@ -1,10 +1,9 @@
 const app = require("./helpers/express")
-const serverless = require("serverless-http")
-const { db } = require("./helpers/database")
+const serverless = require("serverless-http") 
 app.post("*", async (req, res) => {
   try { 
-    const success = await db.insert(req.body)
-    res.status(200).json(success)
+ 
+    res.status(200).json({message: "success"})
   } catch (error) {
     return res.status(504).json({ error })
   }
