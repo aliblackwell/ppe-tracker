89f3c5472c317d81766c233527a1e5f23fa71d0d
Alasdair Blackwell <aliblackwell@protonmail.com>
2020-04-14 18:20:56 +0100

    Add welcome message to SMS

--- functions/sms-conversation-start.js
+++ functions/sms-conversation-start.js
@@ -20,41 +20,57 @@ app.get("*", async (req, res, next) => {
     const success = await db.insert(smsBurst)
     req["sms-burst"] = success
     req["sms-bd"] = burstIdentifier
-
     next()
   } catch (error) {
     console.log(error)
-    //next()
     return res.status(504).json({ error: "Already sent today" })
   }
 })
 
 app.get("*", async (req, res) => {
-  const englishDateString = getEnglishDateString()
-  const docs = await db.partitionedList("user", { include_docs: true })
-  const executions = []
-  for (const doc of docs.rows) {
-    // output each document's body
-    if (doc.doc.mobile && doc.doc.readableDate !== englishDateString) {
-      const execution = await twilio.studio.v1.flows(process.env.TWILIO_FLOW_ID).executions.create({
-        to: doc.doc.mobile,
-        from: process.env.TWILIO_MESSAGING_SERVICE_SID,
-        parameters: {
+  try {
+    const englishDateString = getEnglishDateString()
+    const docs = await db.partitionedList("user", { include_docs: true })
+    const executions = []
+    const updated_docs = []
+    for (const doc of docs.rows) {
+      // output each document's body
+      if (doc.doc.mobile && doc.doc.readableDate !== englishDateString) {
+        // they didn't sign up today
+
+        const parameters = {
           environment: twilioEnvironment,
-        },
-      })
-      executions.push(execution)
+          firstTime: doc.doc.texted ? "no" : "yes",
+        }
+
+        const execution = await twilio.studio.v1
+          .flows(process.env.TWILIO_FLOW_ID)
+          .executions.create({
+            to: doc.doc.mobile,
+            from: process.env.TWILIO_MESSAGING_SERVICE_SID,
+            parameters,
+          })
+        executions.push(execution)
+        const updated_doc = { ...doc.doc }
+        updated_doc.texted = true
+        updated_docs.push(updated_doc)
+      }
     }
-  }
 
-  const newDoc = {
-    _rev: req["sms-burst"].rev,
-    _id: req["sms-bd"],
-    executions,
+    const newDoc = {
+      _rev: req["sms-burst"].rev,
+      _id: req["sms-bd"],
+      executions,
+    }
+    updated_docs.push(newDoc)
+    const success = await db.bulk({ docs: updated_docs })
+    res.json(success)
+  } catch (error) {
+    res.status(504).json({
+      message: "Error sending messages and writing to DB. Please check twilio logs",
+      error,
+    })
   }
-  const success = await db.insert(newDoc)
-
-  res.json(success)
 })
 
 const handler = serverless(app)
